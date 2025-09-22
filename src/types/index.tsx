b42a1ee4b6da1b60354ed3781d4df117b6a67f31
Corentin Deldalle <Corentin.Deldalle-CIC@ibm.com>
2025-09-22 22:15:32 +0200

    merge

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -1,6 +1,4 @@
 export type { Restaurant } from "./restaurant";
-export type { Bar } from "./bar";
-export type { Activity } from "./activity";
 export type { Place } from "./place";
 export type { Image } from "./image";
 export type { Menu } from "./menu";
