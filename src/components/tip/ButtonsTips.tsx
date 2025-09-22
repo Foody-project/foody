94aa375e6d06b24fb0c76e4d650c3ec4fcb04a46
Corentin Deldalle <Corentin.Deldalle-CIC@ibm.com>
2025-09-22 16:54:51 +0200

    connect front with backend

--- src/components/tip/ButtonsTips.tsx
+++ src/components/tip/ButtonsTips.tsx
@@ -4,11 +4,25 @@ import { Export, Pencil, Heart, Warning } from "@phosphor-icons/react";
 import { Button } from "@/components/ui/button";
 import { ShareModal } from "./ShareModal";
 import { useState } from "react";
+import { useAddFavorite } from "@/hooks/places/useSavePlace";
+import { useDeleteFavorite } from "@/hooks/places/useDeleteSavingPlace";
+import { Place } from "@/types";
 
-export default function ButtonTips() {
+export default function ButtonTips({ place }: { place: Place }) {
+  const { mutate: addFavorite } = useAddFavorite();
+  const { mutate: deleteFavorite } = useDeleteFavorite();
+
   const [saved, setSaved] = useState(false);
 
-  const toggleSaved = () => setSaved(!saved);
+  const handleClick = () => {
+    if (saved === false) {
+      addFavorite({ userId: 1, placeId: place.id });
+      setSaved(!saved);
+    } else {
+      deleteFavorite({ userId: 1, placeId: place.id });
+      setSaved(!saved);
+    }
+  };
 
   return (
     <div className="flex gap-3">
@@ -24,7 +38,7 @@ export default function ButtonTips() {
       </Button>
       <Button
         variant="outline"
-        onClick={toggleSaved}
+        onClick={handleClick}
         className="hover:bg-red-200 flex items-center gap-2 font-[400] border-[var(--text-orange)]"
       >
         <Heart
