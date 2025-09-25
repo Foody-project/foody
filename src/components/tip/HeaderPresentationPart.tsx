a67ea4dc6e5a947983741d3877fabe09656d2876
CorentinDeldalle19 <deldallecorentin@gmail.com>
2025-09-25 13:10:04 +0200

    Add adress and phone

--- src/components/tip/HeaderPresentationPart.tsx
+++ src/components/tip/HeaderPresentationPart.tsx
@@ -1,6 +1,8 @@
+import { Badge } from "@/components/ui/badge";
 import { DescriptionPart } from "./DescriptionPart";
 import ReviewRecap from "./ReviewRecap";
 import { Place } from "@/types";
+import { House, Phone } from "@phosphor-icons/react";
 
 interface HeaderPresentationProps {
   readonly place?: Place;
@@ -11,6 +13,22 @@ export default function HeaderPresentationPart({
 }: HeaderPresentationProps) {
   return (
     <div className="flex flex-col justify-between mb-10">
+      <div className="flex flex-row gap-3 pb-3">
+        <Badge
+          variant="secondary"
+          className="bg-[var(--text-orange)]/80 font-thin text-[0.8rem]"
+        >
+          <House />
+          {place?.address}
+        </Badge>
+        <Badge
+          variant="secondary"
+          className="bg-[var(--text-orange)]/80 font-thin text-[0.7rem] flex flex-row justify-center"
+        >
+          <Phone size={38} />
+          {place?.phone}
+        </Badge>
+      </div>
       <DescriptionPart place={place} />
       <ReviewRecap place={place} />
     </div>
